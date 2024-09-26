b75f36f60d5553e11c86c8aced874ab8fbd0f6db
Chimwemwe Masona <justplainrodney@gmail.com>
2024-09-26 13:20:07 +0200

    current weather component

---
+++ resources/js/Components/CurrentWeather.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CurrentWeather = ({ data }) => {
+    if (!data) return null;
+
+    return (
+        <div>
+            <h2>Current Weatherr in {data.city_name}, {data.country_code}</h2>
+            <p>Temperature: {data.temp}°C</p>
+            <p>Feels like: {data.app_temp}°C</p>
+            <p>Weather: {data.weather.description}</p>
+            <p>Wind Speed: {data.wind_spd} m/s</p>
+            <p>Humidity: {data.rh}%</p>
+            <p>Air Quality Index: {data.aqi}</p>
+            <p>Observation Time: {data.ob_time}</p>
+        </div>
+    );
+};
